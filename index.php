d34bc9288734b2248526586c102ce2bbda0b58c7
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-05-18 17:08:57 +0200

    Completamento esercizio

--- index.php
+++ index.php
@@ -33,7 +33,8 @@
                 <!-- password -->
                 <div class="mb-3">
                     <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password">
+                    <input type="password" class="form-control" name="pass" id="password" aria-describedby="passwordHelp">
+                    <div id="passwordHelp" class="form-text">We'll never share your password with anyone else.</div>
                 </div>
             
                 <button type="submit" class="btn btn-primary">Invia</button>
--- risposta.php
+++ risposta.php
@@ -1,3 +1,9 @@
+<?php 
+    $strin = $_POST['pass'];
+
+    $stringanuova = str_replace($_POST['pass'], '***', $strin);
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,18 +18,21 @@
 </head>
 <body>
 
-    <!-- Dati ricevuti -->
-    <div class="card" style="width: 18rem;">
-
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Nome: <?php echo $_POST['nome'] . ' (' . strlen($_POST['nome']) . ')' ?></h5>
-            <h5 class="card-title">Cognome: <?php echo $_POST['cognome'] . ' (' . strlen($_POST['cognome']) . ')'?></h5>
+    <div class="container">
+        <div class="row aling-items-center pt-4">
+            <!-- Dati ricevuti -->
+            <div class="card" style="width: 24rem;">
+        
+                <div class="card-body">
+                    <h5 class="card-title">Nome: <?php echo $_POST['nome'] . ' (lenght:' . strlen($_POST['nome']) . ')' ?></h5>
+                    <h5 class="card-title">Cognome: <?php echo $_POST['cognome'] . ' (lenght:' . strlen($_POST['cognome']) . ')'?></h5>
+                    <h5 class="card-title">Password: <?php echo $stringanuova . ' (lenght:' . strlen($_POST['pass']) . ') '
+                         ?></h5>
+                </div>
+            </div>
 
         </div>
     </div>
-
-
 
     <!-- Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
