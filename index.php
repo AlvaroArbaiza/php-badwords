5316108b4fb43df0f80aad7ac10397ae6c9217b8
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-05-18 15:53:30 +0200

    Creazione form e file di risposta

--- index.php
+++ index.php
@@ -14,9 +14,24 @@ $surname = 'Pato';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Form</title>
 </head>
 <body>
+
+    <!-- form -->
+    <form action="risposta.php" method="POST">
+
+        <!-- Nome -->
+        <label for="">Nome</label>
+        <input type="text" placeholder="Inserisci il nome" name="nome">
+
+        <!-- Cognome -->
+        <label for="">Cognome</label>
+        <input type="text" placeholder="Inserisci il cognome" name="cognome">
+
+        <!-- Submit -->
+        <button type="submit">Invia</button>
+    </form>
     
 </body>
 </html>
