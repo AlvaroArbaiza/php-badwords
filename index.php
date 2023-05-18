739cbe120aab85617e12e1cf2c0643829710163b
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-05-18 16:28:52 +0200

    Stampa di dati nel file risposta.php all'invio del form

--- index.php
+++ index.php
@@ -1,37 +1,50 @@
-<?php
-
-$name = 'Lucas';
-
-$surname = 'Pato';
-
-?>
-
-<h1>Mi chiamo el <?php echo $surname . ' ' . $name ?></h1>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
     <title>Form</title>
 </head>
 <body>
 
-    <!-- form -->
-    <form action="risposta.php" method="POST">
+<div class="container pt-4">    
+    <div class="row">
+        <div class="col-6">
 
-        <!-- Nome -->
-        <label for="">Nome</label>
-        <input type="text" placeholder="Inserisci il nome" name="nome">
+            <!-- form -->
+            <form action="risposta.php" method="POST">
+            
+                <!-- nome -->
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" placeholder="Inserisci il nome" name="nome" class="form-control" id="nome">
+                </div>
+            
+                <!-- cognome -->
+                <div class="mb-3">
+                    <label for="cognome" class="form-label">Cognome</label>
+                    <input type="text" placeholder="Inserisci il cognome" name="cognome" class="form-control" id="cognome">
+                </div>
+            
+                <!-- password -->
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password">
+                </div>
+            
+                <button type="submit" class="btn btn-primary">Invia</button>
+            </form>
+        </div>
+    </div>
+</div>
 
-        <!-- Cognome -->
-        <label for="">Cognome</label>
-        <input type="text" placeholder="Inserisci il cognome" name="cognome">
 
-        <!-- Submit -->
-        <button type="submit">Invia</button>
-    </form>
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     
 </body>
 </html>
--- risposta.php
+++ risposta.php
@@ -4,9 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
     <title>Risposta</title>
 </head>
 <body>
-    <p>Ciao come butta <?php echo $_POST['nome'] . ' ' . $_POST['cognome'] ?>?</p>
+
+    <!-- Dati ricevuti -->
+    <div class="card" style="width: 18rem;">
+
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Nome: <?php echo $_POST['nome'] . ' (' . strlen($_POST['nome']) . ')' ?></h5>
+            <h5 class="card-title">Cognome: <?php echo $_POST['cognome'] . ' (' . strlen($_POST['cognome']) . ')'?></h5>
+
+        </div>
+    </div>
+
+
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    
 </body>
 </html>
